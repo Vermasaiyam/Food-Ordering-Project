beee87893f40ba4c729acad86c6d3e8ae8c88ba3
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-29 13:14:24 +0530

    db connected

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,9 @@
-const mongodbUrl = "mongodb+srv://vermasaiyam9:<db_password>@cluster0.isovh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoose = require("mongoose");
+
+const mongodbUrl = "mongodb+srv://vermasaiyam9:p1MiV0ZnPDmthLm9@cluster0.isovh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+async function connectDB() {
+    return mongoose.connect(mongodbUrl);
+}
+
+module.exports = connectDB;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const { app } = require(".");
+const connectDB = require("./config/db.js");
 
 const PORT = 8000;
 
 app.listen(PORT, async ()=>{
+    await connectDB();
     console.log('server is running on ',PORT);
 })
