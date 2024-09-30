6b87297d8fdc80243d98a9e4c2c95f2b6b2aad06
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-30 21:27:42 +0530

    authentication completed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const homeRouter = require('./routes/homeRouter');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use('/', homeRouter);
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use("/auth", authRoutes);
 
 module.exports = {app};
